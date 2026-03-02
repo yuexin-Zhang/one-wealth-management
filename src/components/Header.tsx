2c93fbe84e576d182e0546aa3562640d4bc87a63
yuexin-Zhang <zyxfwy@gmail.com>
2026-03-02 17:03:37 +0800

    Fix image path for GitHub Pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="flex items-center gap-3">
         {/* HSBC Logo */}
         <img 
-          src="/hsbc_icon.jpg" 
+          src="./hsbc_icon.jpg" 
           alt="HSBC" 
           className="w-10 h-10 object-contain"
         />
